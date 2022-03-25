839e4e60e653652dbda6655024192f6b3b880bd0
Jomarcelino <101756056+Jomarcelino@users.noreply.github.com>
2022-03-25 13:30:44 -0300

    Add files via upload

---
+++ rihu/rihu/Classe/CalculadoraBasica.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rihu.Classe
+{
+    class CalculadoraBasica
+    {
+        private decimal _num1;
+        decimal _num2;
+        decimal _resultado;
+
+        public decimal Num1
+        {
+            get => _num1;
+            set => _num1 = value;
+        }
+        public decimal Num2
+        {
+            get => _num2;
+            set => _num2 = value;
+        }
+        public decimal Resultado
+        {
+            get => _resultado;
+            set => _resultado = value;
+        }
+
+        public void somar()
+        {
+            _resultado = _num1 + _num2;
+        }
+    }
+}
---
+++ rihu/rihu/Form1.cs
@@ -0,0 +1,40 @@
+﻿using rihu.Classe;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rihu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CmbOPERAÇÕES_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CmbOPERAÇÕES_TextChanged(object sender, EventArgs e)
+        {
+            CalculadoraBasica objCalc = new CalculadoraBasica();
+            switch (cmbOPERAÇÕES.Text)
+            {
+                case "Mais":
+                    objCalc.Num1 = decimal.Parse(txtN1.Text);
+                    objCalc.Num2 = decimal.Parse(txtN2.Text);
+                    objCalc.somar();
+                    lblResultado.Text = objCalc.Resultado.ToString();
+                    break;
+            }
+        }
+    }
+}
